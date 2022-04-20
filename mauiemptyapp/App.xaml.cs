4ac52195883172eb5d4fbd9c73e3037ac3c511c4
holecekp <holecekp@holecekp.com>
2022-04-20 19:13:19 +0200

    Add project files.

---
+++ mauiemptyapp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace mauiemptyapp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ mauiemptyapp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace mauiemptyapp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
